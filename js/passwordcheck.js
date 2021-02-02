a610dd0ad2ca60bbc00c8dbb24f6663077bd16cd
Arno Defauw <arno@delandtsheer.eu>
2021-02-02 10:43:19 +0100

    password referral

--- js/passwordcheck.js
+++ js/passwordcheck.js
@@ -16,7 +16,7 @@ function checkPasswordCEO1(){
     if (document.getElementById('psw').value.toLowerCase() === "temsed") {
         document.getElementById('psw-fout').innerHTML = "psw juist";
         sessionStorage.setItem('pcCEO', true);
-		document.location.href = "/pages-network/pcIT.html";
+		document.location.href = '/pages-network/pcCEO.html';
     } else {
         document.getElementById('psw-fout').innerText = "Dat is een fout wachtwoord!";
     }
@@ -27,7 +27,7 @@ function checkPasswordCEO3(){
     if (document.getElementById('psw').value.toLowerCase() === "ceo3") {
         document.getElementById('psw-fout').innerHTML = "psw juist";
         sessionStorage.setItem('pcCEO', 2);
-        document.location.href = '/pages-network/pcCEO.html';
+        history.back();
     } else {
         document.getElementById('psw-fout').innerText = "Dat is een fout wachtwoord!";
     }
