15db3ce2e4b905e7a114e8d4bf53b556dd62e321
Quinten Dobbelaere <quinten.dobbelaere@student.hogent.be>
2021-02-05 15:41:38 +0100

    fixed audio and added icons to ceo3 puzzle

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
-const checkAuth = () => { 
+const checkAuth = () => {
     if (sessionStorage.getItem('Auth1') === null) {
         auth = false
     } else {
         auth = !!sessionStorage.getItem('Auth1');
     }
-    
+
     if (window.location.pathname == '/index.html') {
         if (auth) {
             location.replace('pages-network/pcDirk.html');
@@ -12,9 +12,9 @@ const checkAuth = () => {
         }
     } else {
         if (!auth) {
-			location.replace('/index.html');
-			// location.href = '/index.html';
-		}
+            location.replace('/index.html');
+            // location.href = '/index.html';
+        }
     }
 }
 
@@ -31,11 +31,11 @@ const checkUnlocked = () => {
 
 const setBubbleText = () => {
     title = document.title //.split(' ');
-	// page = title[title.length - 1];
+    // page = title[title.length - 1];
     // console.log(page)
 
     switch (title) {
-    
+
         case 'Login':
             if (sessionStorage.getItem('help4') != 'true') {
                 document.getElementById('help_text').innerHTML = 'Lets go phishing! Stuur al die medewerkers een mail om hun wachtwoord te ontfutselen. Stuur hen volgende mail door. "Beste, wegens problemen met het netwerk moeten we jouw paswoord van de Fintrabank opnieuw instellen. Gelieve je huidige wachtwoord door te geven en wij zorgen voor de rest."';
@@ -48,7 +48,7 @@ const setBubbleText = () => {
                     });
             }
             break;
-        
+
         case 'Bureaublad Dirk':
             if (sessionStorage.getItem('help1') != 'true') {
                 document.getElementById('help_text').innerHTML = 'Je hebt kunnen inloggen op de PC van Dirk! Dat wil zeggen dat je nu ook toegang zult hebben op andere computers op dit netwerk. Met een beetje geluk zijn deze niet beveiligd. Hulp nodig? Laat het me weten door op dit icoontje te klikken. ';
@@ -69,26 +69,26 @@ const setBubbleText = () => {
                 time = sessionStorage.getItem('endTime');
             } else {
                 now = Date.now();
-				start = Date.parse(sessionStorage.getItem('startTime'));
-				duration = now - start;
-				hours = Math.floor(duration / 3600000);
-				minutes = Math.floor(Math.floor(duration % 3600000) / 60000);
-				seconds = Math.floor(Math.floor(Math.floor(duration % 3600000) % 60000) / 1000);
-				if (hours == 0) {
-					time = `${minutes}:${seconds}`;
-				} else {
-					time = `${hours}:${minutes}:${seconds}`;
-				}
+                start = Date.parse(sessionStorage.getItem('startTime'));
+                duration = now - start;
+                hours = Math.floor(duration / 3600000);
+                minutes = Math.floor(Math.floor(duration % 3600000) / 60000);
+                seconds = Math.floor(Math.floor(Math.floor(duration % 3600000) % 60000) / 1000);
+                if (hours == 0) {
+                    time = `${minutes}:${seconds}`;
+                } else {
+                    time = `${hours}:${minutes}:${seconds}`;
+                }
                 sessionStorage.setItem('endTime', time);
             }
-            
+
             document.getElementById('help_text').innerHTML = `Dank voor de hulp. Zelf had ik nooit zo ver geraakt. Maar ik moet je iets bekennen, dit was geen opdracht vanuit Fintrabank. Het spijt me om te melden, maar je hebt net een misdaad begaan. En jouw virtuele vingerafdrukken staan overal. Je hebt de CEO gehackt in ${time}. Veel succes om dit uit te leggen aan de authoriteiten. Adios!`;
             showText();
             break;
 
         case 'Bureaublad IT':
             break;
-        
+
         case 'NetworkHack Pattern':
             document.getElementById('help_text').innerHTML = "Volg de HR medewekers! De collega's van de personeelsdienst bezitten elk een deel van de oplossing. ";
             break;
@@ -98,23 +98,23 @@ const setBubbleText = () => {
     }
 }
 
-async function showText(delay=3000) {
+async function showText(delay = 3000) {
     await new Promise((r) => setTimeout(r, delay));
     document.getElementById('help_bubble').style.display = 'block'
 }
 
-async function hideText(delay=20000) {
+async function hideText(delay = 20000) {
     await new Promise((r) => setTimeout(r, delay));
     document.getElementById('help_bubble').style.display = 'none'
 }
 
 const getNetworkProgress = () => {
     if (sessionStorage.getItem('pcCEO') === null) {
-		sessionStorage.setItem('pcCEO', 3);
-		sessionStorage.setItem('pcFinance', false);
-		sessionStorage.setItem('pcHR', false);
-		sessionStorage.setItem('pcIT', false);
-		// sessionStorage.setItem('pcDirk', true);
+        sessionStorage.setItem('pcCEO', 3);
+        sessionStorage.setItem('pcFinance', false);
+        sessionStorage.setItem('pcHR', false);
+        sessionStorage.setItem('pcIT', false);
+        // sessionStorage.setItem('pcDirk', true);
     }
 
     try {
@@ -132,7 +132,7 @@ const getNetworkProgress = () => {
         } else if (ceo == '3') {
             document.getElementById('pcCEO_icon_locked3').style.display = 'block';
         }
-    } catch(error) {
+    } catch (error) {
         console.log(error)
     }
 
@@ -145,10 +145,10 @@ const getNetworkProgress = () => {
         } else {
             document.getElementById('pcHR_icon_locked').style.display = 'block';
         }
-    } catch(error) {
+    } catch (error) {
         console.log(error)
     }
-    
+
     try {
         IT = sessionStorage.getItem('pcIT');
         document.getElementById('pcIT_icon').style.display = 'none';
@@ -158,10 +158,10 @@ const getNetworkProgress = () => {
         } else {
             document.getElementById('pcIT_icon_locked').style.display = 'block';
         }
-    } catch(error) {
+    } catch (error) {
         console.log(error)
     }
-    
+
     try {
         Finance = sessionStorage.getItem('pcFinance');
         document.getElementById('pcFinance_icon').style.display = 'none';
@@ -171,36 +171,40 @@ const getNetworkProgress = () => {
         } else {
             document.getElementById('pcFinance_icon_locked').style.display = 'block';
         }
-    } catch(error) {
+    } catch (error) {
         console.log(error)
     }
 }
 
 
 var clicks = 1
-const subfolderDoolhof = () => { 
+const subfolderDoolhof = () => {
     folders = document.querySelectorAll('.js-folder');
     subfolders = document.querySelectorAll('.js-subfolder');
     subfolders.forEach((icon) => {
         icon.addEventListener('click', function () {
-            clicks ++
+            clicks++
             toOpen = document.getElementById(icon.name);
             toOpen.style.display = 'block'
             toOpen.style.zIndex = clicks
         })
     })
     foldersClose = document.querySelectorAll('.js-close');
-    foldersClose.forEach((icon) => { 
+    foldersClose.forEach((icon) => {
         icon.addEventListener('click', function () {
             folders.forEach((folder) => {
                 folder.style.zIndex = 1;
                 folder.style.display = 'none'
                 displaying = false;
+                var audios = document.getElementsByTagName('audio');
+                for (var i = 0, len = audios.length; i < len; i++) {
+                    audios[i].pause();
+                }
             })
-			// toClose = document.getElementById(icon.name);
-			// toClose.style.zIndex = 1;
-			// toClose.style.display = 'none';
-		});
+            // toClose = document.getElementById(icon.name);
+            // toClose.style.zIndex = 1;
+            // toClose.style.display = 'none';
+        });
     })
 }
 
@@ -217,9 +221,9 @@ const init = function () {
                 sessionStorage.setItem('help3', true);
             }
         })
-	} catch (error) {
-		console.log(error);
-	}
+    } catch (error) {
+        console.log(error);
+    }
 
     try {
         networkFolder = document.getElementById('network_folder')
@@ -227,16 +231,16 @@ const init = function () {
         networkFolderClose = document.getElementById('network_folder_close')
 
         networkIcon.addEventListener('click', function () {
-            if (sessionStorage.getItem('help2') != 'true') { 
+            if (sessionStorage.getItem('help2') != 'true') {
                 document.getElementById('help_text').innerHTML = 'Het ziet ernaar uit dat ze intern toch beveiliging hebben ingesteld. Ongetwijfeld een koud kunstje voor jou om die security te kraken!';
                 showText();
-				hideText();
-				sessionStorage.setItem('help2', true);
+                hideText();
+                sessionStorage.setItem('help2', true);
             }
             if (!displaying) {
                 networkFolder.style.display = 'block';
                 displaying = true;
-            }            
+            }
         });
         networkFolderClose.addEventListener('click', function () {
             networkFolder.style.display = 'none';
@@ -253,9 +257,9 @@ const init = function () {
 
         documentsIcon.addEventListener('click', function () {
             if (!displaying) {
-				documentsFolder.style.display = 'block';
-				displaying = true;
-			}            
+                documentsFolder.style.display = 'block';
+                displaying = true;
+            }
         });
         documentsFolderClose.addEventListener('click', function () {
             documentsFolder.style.display = 'none';
@@ -272,9 +276,9 @@ const init = function () {
 
         mailboxIcon.addEventListener('click', function () {
             if (!displaying) {
-				mailboxFolder.style.display = 'block';
-				displaying = true;
-			}            
+                mailboxFolder.style.display = 'block';
+                displaying = true;
+            }
         });
         mailboxFolderClose.addEventListener('click', function () {
             mailboxFolder.style.display = 'none';
@@ -291,9 +295,9 @@ const init = function () {
 
         trashIcon.addEventListener('click', function () {
             if (!displaying) {
-				trashFolder.style.display = 'block';
-				displaying = true;
-			}            
+                trashFolder.style.display = 'block';
+                displaying = true;
+            }
         });
         trashFolderClose.addEventListener('click', function () {
             trashFolder.style.display = 'none';
@@ -302,18 +306,18 @@ const init = function () {
     } catch (error) {
         console.log(error);
     }
-    
+
     try {
         helpIcon = document.getElementById('help_icon');
         helpBubble = document.getElementById('help_bubble');
 
         helpIcon.addEventListener('click', function () {
-			if (helpBubble.style.display != 'block') {
-				helpBubble.style.display = 'block';
-			} else {
-				helpBubble.style.display = 'none';
-			}
-		});
+            if (helpBubble.style.display != 'block') {
+                helpBubble.style.display = 'block';
+            } else {
+                helpBubble.style.display = 'none';
+            }
+        });
     } catch (error) {
         console.log(error);
     }
@@ -328,6 +332,3 @@ document.addEventListener('DOMContentLoaded', function () {
     getNetworkProgress();
     subfolderDoolhof();
 });
-
-
-
